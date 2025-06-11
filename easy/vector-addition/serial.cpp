310f5ff67e555d81ca0ddf5901ba3737b7ec848c
Almir Neto <almirneto338@gmail.com>
2025-06-11 16:35:56 -0300

    adding vector addition serial

--- easy/vector-addition/serial.cpp
+++ easy/vector-addition/serial.cpp
@@ -1,8 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <time.hpp>
+#include <vector>
 
-void vector_addition(float *a, float *b, float *c, int n) {
-    for (int i = 0; i < n; i++) {
+void vector_addition(std::vector<float> a, std::vector<float> b,
+                     std::vector<float> c, unsigned int n) {
+    for (unsigned int i = 0; i < n; i++) {
         c[i] = a[i] + b[i];
     }
 }
@@ -10,22 +14,29 @@ void vector_addition(float *a, float *b, float *c, int n) {
 int main() {
     srand(time(0));
 
-    int N = 100000000;
+    unsigned int N = 100000000;
 
-    float *a = (float *)malloc(sizeof(float) * N);
-    float *b = (float *)malloc(sizeof(float) * N);
-    float *c = (float *)malloc(sizeof(float) * N);
+    std::vector<float> a(N);
+    std::vector<float> b(N);
+    std::vector<float> c(N);
 
-    for (int i = 0; i < N; i++) {
+    std::cout << "-> Randomization A and B" << std::endl;
+
+    for (unsigned int i = 0; i < N; i++) {
         a[i] = (float)rand() / RAND_MAX;
         b[i] = (float)rand() / RAND_MAX;
     }
 
-    vector_addition(a, b, c, N);
+    std::cout << "-> Adding vectors A and B into C" << std::endl;
 
-    free(a);
-    free(b);
-    free(c);
+    auto start = std::chrono::high_resolution_clock::now();
+    vector_addition(a, b, c, N);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "-> Vectors A and B added into C" << std::endl;
+
+    std::cout << "-> Duration: " << getElapsedTime(start, end) << " ms"
+              << std::endl;
 
     return 0;
 }
