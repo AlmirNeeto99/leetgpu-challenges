c703ba818fb776281b53c9ded987d960fce237fd
Almir Neto <almirneto338@gmail.com>
2026-03-12 10:28:29 -0300

    adding a sycl vector addition version

--- src/easy/vector-addition/sycl.cpp
+++ src/easy/vector-addition/sycl.cpp
@@ -6,9 +6,36 @@
 using namespace sycl;
 
 int main() {
-    queue queue;
-    device device = queue.get_device();
+    queue q;
+    device device = q.get_device();
     printDeviceInfo(device);
+
+    const unsigned int N = 4096;
+
+    float* a = malloc_shared<float>(N, q);
+    float* b = malloc_shared<float>(N, q);
+    float* c = malloc_shared<float>(N, q);
+
+    for (int i = 0; i < N; i++) {
+        a[i] = static_cast<float>(i);
+        b[i] = static_cast<float>(i * 2);
+    }
+
+    q.parallel_for(range<1>(N), [=](id<1> i) { c[i] = a[i] + b[i]; }).wait();
+
+    bool success = true;
+    for (int i = 0; i < N; i++) {
+        if (c[i] != i + (i * 2)) {
+            success = false;
+            break;
+        }
+    }
+
+    std::cout << (success ? "Success!" : "Verification Failed.") << "\n";
+
+    free(a, q);
+    free(b, q);
+    free(c, q);
 
     return 0;
 }
