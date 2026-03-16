2bea59afdb7bc8dbdc05cb5fb74b03eb2d4eb785
Almir Neto <almirneto338@gmail.com>
2026-03-16 16:12:43 -0300

    style: enhance device info output formatting for clarity

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,15 +8,15 @@ void printDeviceInfo(device device) {
     std::string device_name = device.get_info<info::device::name>();
 
     platform platform = device.get_info<info::device::platform>();
-    std::cout << "Platform: " << platform.get_info<info::platform::name>()
+    std::cout << "-> Platform: " << platform.get_info<info::platform::name>()
               << std::endl;
 
     if (device.has(sycl::aspect::gpu)) {
-        std::cout << "Running on GPU: " << device_name << std::endl;
+        std::cout << "-> Running on GPU: " << device_name << std::endl;
     } else if (device.has(sycl::aspect::cpu)) {
-        std::cout << "Running on CPU: " << device_name << std::endl;
+        std::cout << "-> Running on CPU: " << device_name << std::endl;
     } else {
-        std::cout << "Running on an unknown device: " << device_name
+        std::cout << "-> Running on an unknown device: " << device_name
                   << std::endl;
     }
 }
