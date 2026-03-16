45e1eac81d3958272e22983dba5e3e5dd47fa0ef
Almir Neto <almirneto338@gmail.com>
2026-03-16 16:46:10 -0300

    feat: add syclElapsedTime function for measuring SYCL event execution time

--- include/time.hpp
+++ include/time.hpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <sycl/sycl.hpp>
 
 int64_t getElapsedTime(std::chrono::time_point<std::chrono::system_clock,
                                                std::chrono::duration<double>>
@@ -8,4 +9,12 @@ int64_t getElapsedTime(std::chrono::time_point<std::chrono::system_clock,
                            end) {
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
         .count();
+}
+
+double syclElapsedTime(sycl::event event) {
+    uint64_t start =
+        event.get_profiling_info<sycl::info::event_profiling::command_start>();
+    uint64_t end =
+        event.get_profiling_info<sycl::info::event_profiling::command_end>();
+    return (end - start) / 1e6;
 }
