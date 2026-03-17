e66e07376b23612bf83856f8536b9adef95cbb8d
Almir Neto <almirneto338@gmail.com>
2026-03-17 18:32:16 -0300

    chore: improving cuda template file

--- templates/template.cu
+++ templates/template.cu
@@ -1,17 +1,76 @@
 #include <cuda_runtime.h>
 
 #include <iostream>
+#include <vector>
 
-using namespace std;
-
-__global__ void myKernel(global int *a, global int *b, global int *c, int n) {
-    int i = threadIdx.x;
-    if (i < n) {
+__global__ void kernel(float* A, float* B, float* C, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N) {
+        C[i] = A[i] + B[i];
     }
 }
 
 int main() {
-    cout << "Hello from CUDA (from CPU)" << endl;
+    srand(99);
+
+    int N = 100000000;
+
+    size_t size = sizeof(float) * N;
+
+    std::vector<float> a(N);
+    std::vector<float> b(N);
+    std::vector<float> c(N);
+
+    std::cout << "-> Randomizing A and B" << std::endl;
+
+    for (int i = 0; i < N; i++) {
+        a[i] = (float)rand() / RAND_MAX;
+        b[i] = (float)rand() / RAND_MAX;
+    }
+
+    float *d_a, *d_b, *d_c;
+
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
+
+    cudaMemcpy(d_a, a.data(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b.data(), size, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int numberOfBlocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    std::cout << "-> Running with " << numberOfBlocks << " blocks!"
+              << std::endl;
+    std::cout << "-> Running with " << threadsPerBlock << " threads per block!"
+              << std::endl;
+
+    cudaEvent_t start, end;
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+
+    float elapsed = 0.0f;
+
+    cudaEventRecord(start, 0);
+    std::cout << "Calling kernel passing A, B and C" << std::endl;
+    kernel<<<numberOfBlocks, threadsPerBlock>>>(d_a, d_b, d_c, N);
+
+    cudaEventRecord(end, 0);
+
+    cudaEventSynchronize(end);
+    cudaEventElapsedTime(&elapsed, start, end);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(end);
+
+    std::cout << "-> Elapsed: " << elapsed << " ms" << std::endl;
+
+    cudaMemcpy(c.data(), d_c, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
 
     return 0;
 }
