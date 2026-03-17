757f39ded810d857586f0fff092e0517fca1d48f
Almir Neto <almirneto338@gmail.com>
2026-03-16 22:36:48 -0300

    feat: adding sycl RELU implementation

---
+++ src/easy/relu/sycl.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <sycl/sycl.hpp>
+#include <time.hpp>
+#include <utils.hpp>
+#include <vector>
+
+using namespace sycl;
+
+int main() {
+    srand(99);
+    queue q(default_selector_v, {property::queue::enable_profiling()});
+    device device = q.get_device();
+    printDeviceInfo(device);
+
+    const unsigned int N = 100000000;
+
+    std::vector<float> a(N);
+
+    for (unsigned int i = 0; i < N; i++) {
+        a[i] = myRand();
+    }
+
+    {
+        range<1> r(N);
+        buffer<float> b_a(a.data(), r);
+
+        event event = q.submit([&](handler& h) {
+            accessor a_acc = b_a.get_access<access_mode::read_write>(h);
+            h.parallel_for(r, [=](id<1> i) { a_acc[i] = fmax(0, a_acc[i]); });
+        });
+        event.wait();
+
+        std::cout << "-> Duration: " << syclElapsedTime(event) << " ms"
+                  << std::endl;
+    }
+
+    bool success = true;
+    for (unsigned int i = 0; i < N; i++) {
+        if (a[i] < .0f) success = false;
+    }
+
+    std::cout << (success ? "Success!" : "Verification Failed.") << "\n";
+
+    return 0;
+}
