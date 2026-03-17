4312c9bbd99fb96d916e0268db3dbaafad953972
Almir Neto <almirneto338@gmail.com>
2026-03-17 17:27:40 -0300

    fix: correct leaky_relu function to output results in second vector

--- src/easy/leaky-relu/serial.cpp
+++ src/easy/leaky-relu/serial.cpp
@@ -6,10 +6,11 @@
 #include <utils.hpp>
 #include <vector>
 
-void leaky_relu(std::vector<float>& a, const float alpha, unsigned int n) {
+void leaky_relu(const std::vector<float> a, std::vector<float>& b,
+                const float alpha, unsigned int n) {
     for (unsigned int i = 0; i < n; i++) {
         if (a[i] <= .0f) {
-            a[i] = alpha * a[i];
+            b[i] = alpha * a[i];
         }
     }
 }
@@ -20,7 +21,7 @@ int main() {
     unsigned int N = 100000000;
     const float alpha = 0.01;
 
-    std::vector<float> a(N);
+    std::vector<float> a(N), b(N);
 
     std::cout << "-> Randomizing A" << std::endl;
 
@@ -32,16 +33,17 @@ int main() {
     std::cout << "-> Applying Leaky RELU over A" << std::endl;
 
     auto start = std::chrono::high_resolution_clock::now();
-    leaky_relu(a, alpha, N);
+    leaky_relu(a, b, alpha, N);
     auto end = std::chrono::high_resolution_clock::now();
 
     std::cout << "-> Duration: " << getElapsedTime(start, end) << " ms"
               << std::endl;
 
-    bool success = false;
-    std::cout << a[0] << std::endl;
-    if (a[0] == -1.f * alpha) {
-        success = true;
+    bool success = true;
+    for (unsigned int i = 0; i < N; i++) {
+        if (a[i] <= 0.f && a[i] * alpha != b[i]) {
+            success = false;
+        }
     }
     std::cout << (success ? "Success" : "Failed") << std::endl;
 
